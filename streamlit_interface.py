e3095af8740035cfab94f382a7170bdd63178dac
jakobwes <jakobwes@aol.com>
2021-10-27 01:56:59 +0100

    Caching gave issues, so removed it. Option of sampling method

--- streamlit_interface.py
+++ streamlit_interface.py
@@ -10,48 +10,42 @@ print("Loading tokenizer")
 tokenizer = AutoTokenizer.from_pretrained(model_saving_dir_path)  
 print("Tokenizer and model loaded")
 
-@st.cache()
-def run_text_completion(input_txt, model, tokenizer):
+
+def run_text_completion(input_txt, model, tokenizer, option):
 	input_ids = tokenizer.encode(input_txt, return_tensors='pt')
 
-	
-	# Beam-sampling
-	#beam_outputs = model.generate(
-	#    input_ids, 
-	#    max_length=200, 
-	#    num_beams=1, #3
-	#    no_repeat_ngram_size=2, 
-	#    num_return_sequences=1, #3 
-	#    early_stopping=True
-	#)
-
-	# now we have 3 output sequences
-	#return tokenizer.decode(beam_outputs[0], skip_special_tokens=True).lower()
-	#for i, beam_output in enumerate(beam_outputs):
-	#  print("{}: {}".format(i, tokenizer.decode(beam_output, skip_special_tokens=True)))
-
-	# tokenizer.decode(beam_outputs[0], skip_special_tokens=True)
-
-	#	# Top p sampling
-	sample_output = model.generate(
-	    input_ids, 
-	    do_sample=True, 
-	    max_length=200, 
-	    top_p=0.92, 
-	    top_k=0
-	)
-
-
-	return tokenizer.decode(sample_output[0], skip_special_tokens=True)
+	if option == "Beam sampling":
+		beam_outputs = model.generate(
+			input_ids, 
+			max_length=200, 
+			num_beams=1, #3
+			no_repeat_ngram_size=2, 
+			num_return_sequences=1, #3 
+			early_stopping=True
+		)
+		return tokenizer.decode(beam_outputs[0], skip_special_tokens=True)
+	else:
+		#	# Top p sampling
+		sample_output = model.generate(
+		    input_ids, 
+		    do_sample=True, 
+		    max_length=50, 
+		    top_p=0.92, 
+		    top_k=0
+		)
+		return tokenizer.decode(sample_output[0], skip_special_tokens=True)
 
 
 #st.title('Finance-GPT2')
 st.title("Autocomplete your texts in finance lingo")
 
+option = st.selectbox('Text generating method', ('Beam sampling', 'Top p sampling'))
+
 input_txt = st.text_area('Text to complete', '''The report published by ''')
 
 if st.button('Run'):
-	st.write(run_text_completion(input_txt, model, tokenizer))
+	output_txt = run_text_completion(input_txt, model, tokenizer, option)
+	st.write(output_txt)
 
 
 
