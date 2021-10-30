6c6fa98b5feb1e3a6dfcc1584a9c3c46cf8f5b4d
jakobwes <jakobwes@aol.com>
2021-10-30 15:31:52 +0100

    Added slider for num sequencesand link for samplign method

--- streamlit_interface_api.py
+++ streamlit_interface_api.py
@@ -3,7 +3,7 @@ import requests
 import time
 
 API_URL = "https://api-inference.huggingface.co/models/jakobwes/finance-gpt2"
-headers = {"Authorization": st.secrets["Authorization"]}
+headers = {"Authorization": "Bearer api_CIRGlyQhCuBOhpPcyiUQwtDgAoGvXjlLwp"}
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
@@ -15,13 +15,15 @@ def query(payload):
 #st.title('Finance-GPT2')
 st.title("Autocomplete your texts in finance lingo")
 
-st.write("Top k sampling is used. Please use the sliders on the left to control the sampling :) ")
+st.write("Different sampling methods as [Top-K or Top-P sampling](https://huggingface.co/blog/how-to-generate) can be used. Please use the sliders on the left to control the sampling :) ")
 
 max_length = st.sidebar.slider("Max Length", min_value = 10, max_value=100, value = 50)
 
 top_k = st.sidebar.slider("Top-k", min_value = 0, max_value=500, value = 60)
 
 top_p = st.sidebar.slider("Top-p", min_value = 0.0, max_value=1.0, step = 0.05, value = 0.92)
+
+num_return_sequences = st.sidebar.slider("Number of generated sequences", min_value = 1, max_value=3, step = 1, value = 1)
 
 
 
@@ -35,24 +37,23 @@ if st.button('Run'):
 				"top_k" :  top_k,
 				"top_p" : top_p,
 				"max_new_tokens" : max_length,
-				"use_cache": False
+				"use_cache": False,
+				"num_return_sequences" : num_return_sequences
 			}
 		}
 	)
 
-	if isinstance(query_response, list):
-		query_response = query_response[0]
-
-
-	if "error" in query_response.keys():
-		st.write(query_response)
-		if "estimated_time" in query_response.keys():
-			my_bar = st.progress(0)
-			for pct_complete in range(100):
-				time.sleep(query_response["estimated_time"]/100)
-				my_bar.progress(pct_complete + 1)
+	if not isinstance(query_response, list):
+		if "error" in query_response.keys():
+			st.write(query_response)
+			if "estimated_time" in query_response.keys():
+				my_bar = st.progress(0)
+				for pct_complete in range(100):
+					time.sleep(query_response["estimated_time"]/100)
+					my_bar.progress(pct_complete + 1)
 	else:
-		st.write(query_response["generated_text"])
+		for response in query_response:
+			st.write(response["generated_text"])
 
 
 
